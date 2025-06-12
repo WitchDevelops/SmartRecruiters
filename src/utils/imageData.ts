33af6975167f64484a3805b43b1b18eafdd96110
WitchDevelops <d.n.wojewska@gmail.com>
2025-06-12 19:33:04 +0200

    feat: bento grid layout with auto rows

--- src/utils/imageData.ts
+++ src/utils/imageData.ts
@@ -21,16 +21,16 @@ export const images: Image[] = [
   },
   {
     id: 3,
+    src: new URL('../assets/images/media-4.png', import.meta.url).href,
+    alt: 'Description of Image 4',
+    type: 'image',
+  },
+  {
+    id: 4,
     src: new URL('../assets/images/media-3.png', import.meta.url).href,
     alt: 'Description of Image 3',
     type: 'video',
     videoUrl: 'https://www.youtube.com/watch?v=x6iyz1AQhuU',
-  },
-  {
-    id: 4,
-    src: new URL('../assets/images/media-4.png', import.meta.url).href,
-    alt: 'Description of Image 4',
-    type: 'image',
   },
   {
     id: 5,
